7653a822ea2fc3ed14eaa8a9fb72fb09d05dbb6a
Valera Shutylev <v.shutylev@gmail.com>
2020-12-23 19:15:26 +0400

    Extend User POJO with Entity type

--- src/main/java/com/edu/mano/covidregistration/domain/User.java
+++ src/main/java/com/edu/mano/covidregistration/domain/User.java
@@ -2,17 +2,31 @@ package com.edu.mano.covidregistration.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     private String firstName;
     private String middleName;
     private String lastName;
+
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dateOfBirth;
     private String insuranceNumber;
     private String phoneNumber;
