9380efaf340d64114d62e1f80e2c7014c896f5a3
Valera Shutylev <v.shutylev@gmail.com>
2020-12-23 19:19:27 +0400

    Extend User POJO with Entity type

--- src/main/java/com/edu/mano/covidregistration/repository/UserRepository.java
+++ src/main/java/com/edu/mano/covidregistration/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.edu.mano.covidregistration.repository;
 
+import com.edu.mano.covidregistration.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository {
+public interface UserRepository extends CrudRepository<User,Long> {
+
 }
