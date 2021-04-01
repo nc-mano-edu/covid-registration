b8042afda61d486b4e8ca8ff8ffd9417613cc3d2
vash0514 <valerii.shutylev@netcracker.com>
2021-04-01 19:30:16 +0400

    Feature: Add covid-registration-client

--- src/main/java/com/edu/mano/covidregistration/controller/UserController.java
+++ src/main/java/com/edu/mano/covidregistration/controller/UserController.java
@@ -5,17 +5,13 @@ import com.edu.mano.covidregistration.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@CrossOrigin //todo: this is WA. Need to fix on the frontend part
 @RequestMapping(value = "/users")
 public class UserController {
     private final UserService userService;
