5de84baad0b76790360bc5055664e9d3e7e690dc
Alexander <pichulev23071999@gmail.com>
2021-04-09 16:28:06 +0400

    Create UserRequest and Symptom entities

---
+++ covid-registration-client/src/app/userRequest/models/userRequest.model.ts
@@ -0,0 +1,13 @@
+import {User} from "../user/user.model";
+import {Symptom} from "./symptom.model";
+
+export class UserRequest {
+
+  id: string;
+  startDate: string;
+  endDate: string;
+  treatmentState: string;
+  user: User;
+  symptoms: Symptom[];
+
+}
