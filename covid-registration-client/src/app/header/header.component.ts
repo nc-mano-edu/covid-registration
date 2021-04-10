855e55fc443879933e99cb96dc783d26c6c896bf
BloodlessRed <58694725+BloodlessRed@users.noreply.github.com>
2021-04-10 14:24:21 +0400

    Added global styles; Added a footer; Made it possible to fill footer content through TypeScript arrays (contentLeft & contentRight); Changed templates to template urls; Added images for the main page; Changed header to look like the example site; Added home page;

--- covid-registration-client/src/app/header/header.component.ts
+++ covid-registration-client/src/app/header/header.component.ts
@@ -5,17 +5,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-    `.angular-logo {
-        margin: 0 4px 3px 0;
-        height: 35px;
-        vertical-align: middle;
-    }
-    .fill-remaining-space {
-      flex: 1 1 auto;
-    }
-    `
-  ]
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
