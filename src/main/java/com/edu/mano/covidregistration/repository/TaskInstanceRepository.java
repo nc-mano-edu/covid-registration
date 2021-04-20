6916b9c91a3e750ecdc777e0a0f7048336eed0cc
slbor <sl.borisov.42@outlook.com>
2021-04-20 18:26:26 +0400

    JobScheduler is simplified

--- src/main/java/com/edu/mano/covidregistration/repository/TaskInstanceRepository.java
+++ src/main/java/com/edu/mano/covidregistration/repository/TaskInstanceRepository.java
@@ -20,18 +20,4 @@ public interface TaskInstanceRepository extends CrudRepository<TaskInstance, Lon
 
     @Query("SELECT ti FROM task_instances ti WHERE ti.isActive = true")
     List<TaskInstance> findAllByActiveIsTrue();
-
-    @Query(value = "SELECT t.task_instance_id,\n" +
-            "       t.created_time,\n" +
-            "       t.finished_time,\n" +
-            "       t.is_active,\n" +
-            "       t.request_id,\n" +
-            "       t.task_id\n" +
-            "FROM (SELECT row_number() over (PARTITION BY t.request_id) AS r,\n" +
-            "             t.*\n" +
-            "      FROM task_instances t\n" +
-            "      WHERE t.task_id = ?1) t\n" +
-            "WHERE t.r = 1",
-            nativeQuery = true)
-    List<TaskInstance> findOneForEachRequestByTaskId(Long taskId);
 }
--- src/main/java/com/edu/mano/covidregistration/repository/mock/MockTaskInstanceRepository.java
+++ src/main/java/com/edu/mano/covidregistration/repository/mock/MockTaskInstanceRepository.java
@@ -114,9 +114,4 @@ public class MockTaskInstanceRepository implements TaskInstanceRepository {
     public List<TaskInstance> findAllByActiveIsTrue() {
         return new ArrayList<>();
     }
-
-    @Override
-    public List<TaskInstance> findOneForEachRequestByTaskId(Long taskId) {
-        return taskInstances;
-    }
 }
