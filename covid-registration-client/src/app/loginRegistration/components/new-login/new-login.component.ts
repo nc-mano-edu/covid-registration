c2e3cc55bfb13b62c59ff717811626f76b1b95c2
Sergey <sergeyobraztsov.web@gmail.com>
2021-04-30 15:50:41 +0400

    backend registration+login, integration test for login+registration, add recommendation in user request, change model.User UI login + registration

---
+++ covid-registration-client/src/app/loginRegistration/components/new-login/new-login.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegistrationService } from '../../services/registration.service';
+
+@Component({
+  selector: 'app-new-login',
+  templateUrl: './new-login.component.html',
+  styleUrls: ['./new-login.component.css']
+})
+export class NewLoginComponent implements OnInit {
+
+  myLoginForm: FormGroup; 
+  private formSubmitAttempt: boolean;
+  errorMsg = "";
+
+  constructor(
+    private _service: RegistrationService,
+    private _router : Router
+  ) { }
+
+  ngOnInit(): void {
+    this.myLoginForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email, Validators.minLength(6)]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(3)])
+    })
+  }
+
+  isFieldInvalid(field: string) {
+    return (
+      (!this.myLoginForm.get(field).valid && this.myLoginForm.get(field).touched) ||
+      (this.myLoginForm.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
+  
+  onSubmit(){
+    this._service.loginUser(this.myLoginForm.value).subscribe(
+      data => {
+        console.log("response recieved");
+        this._router.navigate(['/home'])
+
+      } ,
+      error =>  {
+        this.errorMsg = "Invalid username and passwor. Please try again"
+        console.log("exception occured");
+      }
+    )
+    this.formSubmitAttempt = true;
+  }
+
+}
