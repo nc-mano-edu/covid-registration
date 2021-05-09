84c9ef5c3dd9674409714542dd4c26eafa955a84
Alexander <pichulev23071999@gmail.com>
2021-05-09 14:49:16 +0400

    Fix broken tests

--- src/main/java/com/edu/mano/covidregistration/jobs/JobScheduler.java
+++ src/main/java/com/edu/mano/covidregistration/jobs/JobScheduler.java
@@ -7,6 +7,7 @@ import com.edu.mano.covidregistration.service.TaskService;
 import com.edu.mano.covidregistration.service.UserRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.support.CronTrigger;
@@ -15,6 +16,7 @@ import java.util.Date;
 
 @Configuration
 @EnableScheduling
+@Profile("!test")
 public class JobScheduler {
 
     private final TaskService taskService;
