8d02237e106e53ca9cc4f904b26842bcae9eb59c
Sergey <sergeyobraztsov.web@gmail.com>
2021-05-19 14:45:22 +0400

    add all buttons to toolbar

--- covid-registration-client/src/app/header/header.component.ts
+++ covid-registration-client/src/app/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { Component, OnInit } from '@angular/core';
+import {Subscription} from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,10 +13,21 @@ export class HeaderComponent implements OnInit {
 
   isLoggedIn$: Observable<boolean>;
 
-  constructor(private authService: AuthService) { }
+  isPatient: boolean;
+  isDoctor: boolean;
+  isAdmin: boolean;
+  private querySubscription: Subscription;
+  userIdFromStorage:string;
+
+  constructor(private authService: AuthService,
+    private _router : Router
+    ) { }
 
   ngOnInit() {
     this.isLoggedIn$ = this.authService.isLoggedIn;
+    this.isPatient = (sessionStorage.getItem("isPatient")=="true");
+    this.isDoctor = (sessionStorage.getItem("isDoctor")=="true");
+    this.isAdmin = (sessionStorage.getItem("isAdmin")=="true");
   }
 
   onLogout() {
@@ -23,6 +36,32 @@ export class HeaderComponent implements OnInit {
 
   getAccountInfo(){
     this.authService.getAccountInfo();
+  }
+
+  getAllTaskInstances(){
+    this._router.navigate(['/task_instances'], {      
+    })
+  }
+
+  getActiveTaskInstances(){
+    this._router.navigate(['/task_instances/active'], {      
+    })
+  }  
+
+  getAllTasks() {    
+    this._router.navigate(['user/'+sessionStorage.getItem('user-id')+ '/tasks'], {      
+    })
+  }
+
+  getActiveTasks(){
+    this._router.navigate(['user/'+ sessionStorage.getItem('user-id')+'/tasks/active'], {      
+    })
+  }
+
+  createRequest(){  
+    this._router.navigate(['/request/create/'], {
+      queryParams: { userId:sessionStorage.getItem('user-id') }
+    })
   }
 
 }
